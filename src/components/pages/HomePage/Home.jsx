e57f24c23a8b4ab15796def523b10d8e9cf7539e
Omar Al-Azzawi <alazzawiomar01@gmail.com>
2021-07-01 19:40:59 +0300

    add Footer and pricing section

--- src/components/pages/HomePage/Home.jsx
+++ src/components/pages/HomePage/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {homeObjFour, homeObjOne, homeObjThree, homeObjTwo} from "./Data";
 import HeroSection from '../HeroSection';
+import Pricing from '../../Pricing';
 
 function Home() {
     return (
@@ -8,6 +9,7 @@ function Home() {
             <HeroSection {...homeObjOne}/>
             <HeroSection {...homeObjTwo}/>
             <HeroSection {...homeObjThree}/>
+            <Pricing />
             <HeroSection {...homeObjFour}/>
         </div>
     )
